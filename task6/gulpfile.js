3161f93708dfdc3aa4f35caef747c62dec394d69
Pavel <95431183+1Checkmate1@users.noreply.github.com>
2021-12-20 09:48:11 +0600

    Update gulpfile.js

--- task6/gulpfile.js
+++ task6/gulpfile.js
@@ -1,18 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/map.css">
-    <title>Document</title>
-</head>
-<body>
-    <div class="map">
-        <img src="./img/1.jpg" alt="img">
-        <a href="#" class="point point1" title="Купить билет в Москву"></a>
-        <a href="#" class="point point2" title="Купить билет в Анкару"></a>
-        <a href="#" class="point point3" title="Купить билет в Мадрид"></a>
-    </div>
-</body>
-</html>
+const gulp = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+const rimraf = require('rimraf');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            port: 9000,
+            baseDir: "build"
+        }
+    });
+
+    gulp.watch('build/**/*').on('change', browserSync.reload)
+});
+
+//Html
+gulp.task('html', () => {
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('build'))
+})
+
+//Styles
+gulp.task('sass', () => {
+    return gulp.src('src/scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass()
+            .on('error', sass.logError))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/css'))
+})
+
+//Clear
+gulp.task('clear', function del(cb) {
+    return rimraf('build', cb);
+});
+
+//Watchers
+gulp.task('watch', function() {
+    gulp.watch('src/index.html', gulp.series('html'));
+    gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series(
+    'clear',
+    gulp.parallel('html', 'sass'),
+    gulp.parallel('watch', 'server')
+    )
+);
